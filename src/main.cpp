19c41862ee124668381bf697b687521015d2e490
Oliver <otad19@gmail.com>
2026-06-19 18:05:38 -0700

    scanning: lexical errors

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,7 @@ int main(int argc, char *argv[]) {
             catch(const std::exception& e)
             {
                 std::cerr << e.what() << '\n';
+                return 65;
             }
         }
         else
